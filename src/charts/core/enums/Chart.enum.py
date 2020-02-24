32cae531dcc09145c9136b22361b36d1de13dc10
Jeffrey Cortés <cortesj3f@gmail.com>
2020-02-23 19:53:41 -0500

    PoC de implementación de las clases de utilidad para enumeraciones.

---
+++ src/charts/core/enums/Chart.enum.py
@@ -0,0 +1,6 @@
+#from src.shared.domain.utils.enums.auto_number import AutoNumber
+from src.shared.domain.utils.enums.auto_name import AutoName
+
+class Chart(AutoName):
+    LINE = ()
+    CURVE = ()
