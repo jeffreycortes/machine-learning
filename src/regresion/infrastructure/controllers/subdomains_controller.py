18125ff513cfafd737a0cfa0650fa1d827fff52c
Jeffrey Cortés <cortesj3f@gmail.com>
2020-02-23 20:13:45 -0500

    se implementa APiController en el controlador de subdominios

--- src/regresion/infrastructure/controllers/subdomains_controller.py
+++ src/regresion/infrastructure/controllers/subdomains_controller.py
@@ -1,8 +1,13 @@
-from ..rest_client.app import app, jsonify
+from .api_controller import ApiController
 from ...domain.lineal_simple.infrastructure.controllers.lineal_simple_controller import LinealSimpleController
 from ...domain.lineal_multiple.infrastructure.controllers.lineal_multiple_controller import LinealMultipleController
 from ...domain.no_lineal.infrastructure.controllers.no_lineal_controller import NoLinealController
 
-LinealSimpleController(app, jsonify)
-LinealMultipleController(app, jsonify)
-NoLinealController(app, jsonify)
+class SubdomainsController(ApiController):
+    def __init__(self):
+        ApiController.__init__(self)
+
+    def _register_route_map(self, app, jsonify):
+        LinealSimpleController(app, jsonify)
+        LinealMultipleController(app, jsonify)
+        NoLinealController(app, jsonify)
