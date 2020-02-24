b583ebba5ed7730ad887d9782f23ac6bf73e8a73
Jeffrey Cortés <cortesj3f@gmail.com>
2020-02-23 19:55:40 -0500

    se implementa llamado a capa de dominio para probar la creación de un gráfico demo

---
+++ src/charts/domain/line_chart.py
@@ -0,0 +1,26 @@
+import matplotlib.pyplot as plt
+from src.shared.domain.utils.img.img_manager import ImageManager
+
+class LineChart:
+    def __init__(self, x = 0, y = 0):
+        self.x = x
+        self.y = y
+        self.xdata = 0
+        self.ydata = 0
+        self.plt = plt
+
+    def plot_default(self):
+        self.plt.plot(self.x, self.ydata,  'bo')
+        self.plt.plot(self.x, self.y, 'r')
+
+    def set_labels(self, xlabel, ylabel):
+        self.plt.xlabel(xlabel)
+        self.plt.ylabel(ylabel)
+
+    def print_to_imgb64(self):
+        imgManager = ImageManager()
+        plt.savefig(imgManager.get_img_buffer(), format='png')
+        return imgManager.get_img_in_base64()
+
+    def show(self):
+        self.plt.show()
