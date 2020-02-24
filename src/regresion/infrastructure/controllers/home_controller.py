455bfd9b3dedb6bc6040f19a213e9e9c5735619b
Jeffrey Cortés <cortesj3f@gmail.com>
2020-02-23 19:59:23 -0500

    Se implementa ApiController en HomeController para adopar el estándar del método register_route_maps.

--- src/regresion/infrastructure/controllers/home_controller.py
+++ src/regresion/infrastructure/controllers/home_controller.py
@@ -1,10 +1,15 @@
-from ..rest_client.app import app, jsonify
+from .api_controller import ApiController
 
-class HomeController:
-    @app.route("/")
-    def index():
-        return jsonify('Hello from Index')
+class HomeController(ApiController):
+    def __init__(self):
+        ApiController.__init__(self)
 
-    @app.route("/api")
-    def indexAPI():
-        return jsonify('Hello from API')
+    def _register_route_map(self, app, jsonify):
+        print('HomeController __register_route_map')
+        @app.route("/")
+        def index():
+            return jsonify('Hello from Index')
+
+        @app.route("/api")
+        def indexAPI():
+            return jsonify('Hello from API')
